6fe86cfc1c5073d4fee4c2751589eae4c09f6370
David <>
2022-10-05 13:17:42 -0400

    added the logic to make the blocks black when hover

--- script.js
+++ script.js
@@ -4,9 +4,15 @@ for (let column = 1; column <= 16; column++) {
     for (let row = 1; row <= 16; row++) {
         const div = document.createElement('div');
         div.classList.add('grid')
-        div.textContent = `.`
         gridContainer.appendChild(div);
     }
 }
 
+const grids = document.querySelectorAll('.grid');
+
+grids.forEach(grid => {
+    grid.addEventListener('mouseover',(e) =>{
+        grid.classList.add('active')
+    })
+});
     
